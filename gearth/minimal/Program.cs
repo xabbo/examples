d935890c95514490d8247421e2d8cafc96dede42
rob <rob@b7c.io>
2024-08-18 18:06:30 +1200

    add minimal g-earth extension

---
+++ gearth/minimal/Program.cs
@@ -0,0 +1,19 @@
+﻿// A minimal G-Earth extension example.
+
+using Xabbo;
+using Xabbo.GEarth;
+using Xabbo.Messages.Flash;
+
+var ext = new GEarthExtension(new GEarthOptions {
+    Title = "Minimal",
+    Description = "A minimal xabbo/gearth example",
+    Author = "b7",
+    Version = "1.0"
+});
+ext.Intercept(In.Chat, e => {
+    e.Packet.Read<int>();
+    if (e.Packet.Read<string>().Contains("ping"))
+        ext.Send(Out.Shout, "pong");
+});
+await ext.RunAsync();
+
